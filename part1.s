805ca3a313ba7087ae4eac39f278f98cd95cc15d
Bikramjit Narwal <bikramjitnarwal@hotmail.com>
2020-04-27 19:54:36 -0400

    Update part1.s

--- part1.s
+++ part1.s
@@ -4,7 +4,7 @@ _start:
 // Question 1(a)
 WAIT_FOR_VSYNC:
 	PUSH {R0-R3, LR}
-	LDR R12, ADDRESS_OF_VGA_CONTROLLER
+	LDR R12, =0xFF203020
 	MOV R0, #1
 	LDR R1, =0xFF20302C
 	STR R0, [R12]
@@ -23,42 +23,42 @@ END:
 // Question 1(b)
 PLOT_PIXEL:
 	// We want the back buffer, so add 4 
+	// R3 = BACK BUFFER
 	LDR R3, [R12, #4]
 	// Shift y coordinate: (y << 10) & x coordinate: (x << 1)
 	LSL R1, #10
 	LSL R0, #1
 	// Now add the shifted y and x coodinates then store it in back buffer
 	ADD R3, R1
-	ADD R3, R3
+	ADD R3, R0
 	STR R2, [R3]
 	MOV PC, LR
 
-ADDRESS_OF_VGA_CONTROLLER: 
-	.word =0xFF203020
-	
+
 // Question 1(c)
 
-CLEAR_SCREEN:
-	PUSH {R0-R3}
-	// We need to double loop to clear screen 
-	MOVE R0, #0
-	MOVE R1, #0
-	MOVE R2, #0
-	MOVE R3, LR
-	
-	X:
-		Y:
-			BL PLOT_PIXEL
-			CMP R1, #0
-			ADD R1, #1
-			BLT Y
-			
-		CMP R0, #320
-		ADD R0, #1
-		BLT X
-		
-	PUSH {R0-R3}
-	BX LR
+// We want to start from 0,0
+MOV R0, #0 
+MOV R1, #0 
+MOV R3, LR 
 
-END_LOOP: B END_LOOP
+X:
+	Y: 
+		BL PLOT_PIXEL
+
+		// Check if you hit the pixel 240 
+		CMP R1, #240
+		// If you dont hit pixel 240, move 1 
+		ADD R1, #1
+		// And keep looping
+		BLT Y
+	
+	// Check if x hits pixel 320 
+	CMP R0, #320
+	// If you dont hit pixel 320, move 1 
+	ADD R0, #1
+	// And keep looping
+	BLT X
+
+MOV PC, LR 
 	
