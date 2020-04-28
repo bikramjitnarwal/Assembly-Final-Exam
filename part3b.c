6d9adaf67327898ec4831f6fb6a4dea761a4d3c0
Bikramjit Narwal <bikramjitnarwal@hotmail.com>
2020-04-28 07:36:01 -0400

    Update part3b.c

--- part3b.c
+++ part3b.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Global variables: Because they will be used everywhere no matter which function it is 
 // ADDRESS FOR KEY
-volatile int *KEY = (int*)0xFF20005C;
+volatile int *KEY_ptr = (int*)0xFF20005C;
 // ADDRESS FOR LED
-volatile int *LED = (int*)(0xFF200000);
+volatile int *LED_ptr = (int*)(0xFF200000);
 // ADDRESS FOR timer status
-volatile int *timerStatus = (int*)(0xFF202000);
+volatile int *timer_status = (int*)(0xFF202000);
 
 // Count variable will help us go left or go right 
-int count = 0;
+int direction_counter = 0;
 // First just initialize left to true since we are going left first 
 bool left = true;
 
 void checkFlag(){
-	while (1)
-	{	
-		// if the flag is set, leave the while loop
-		if ((*timerStatus) & 0b1 == 1)
-		{
+	while (1){	
+		// We can only leave this while loop if the timer gets set 
+		if ((*timer_status) & 0b1 == 1) {
 			break;
 		}
 	}
@@ -27,67 +24,62 @@ void checkFlag(){
 
 void resetCount(){
 	// resets the count flag, sets timer to 25M, starts counter
-	*(timerStatus) = 0;
-	*(timerStatus + 2) = 0x7840;
-	*(timerStatus + 3) = 0x17D;
-	*(timerStatus + 1) = 0b0100;
+	*(timer_status) = 0;
+	*(timer_status + 2) = 0x7840;
+	*(timer_status + 3) = 0x17D;
+	*(timer_status + 1) = 0b0100;
 }
 
 void resetEdgeCapture(){
-	// to reset the edge capture, set 1 to the bit that was triggered by the key press
-	if (*KEY == 0b0001) {
-		*KEY = 1;
-	}
-	else if (*KEY == 0b0010) {
-		*KEY = 1 << 1;
-	}
-	else if (*KEY == 0b0100) {
-		*KEY = 1 << 2;
-	}
-	else if (*KEY == 0b1000) {
-		*KEY = 1 << 3;
+	// Edge capture = reset when we set the value of 1 to the bit which was initiated by the press
+	// of the key 
+	if (*KEY_ptr == 0b0001) {
+		*KEY_ptr = 1;
+	} else if (*KEY_ptr == 0b0010) {
+		*KEY_ptr = 1 << 1;
+	} else if (*KEY_ptr == 0b0100) {
+		*KEY_ptr = 1 << 2;
+	} else if (*KEY_ptr == 0b1000) {
+		*KEY_ptr = 1 << 3;
 	}
 }
 
 void goingLeft(){
 	// Light up LED which you start at 
-	*LED = 1 << count;
+	*LED_ptr = 1 << direction_counter;
 
-	// Move left 
-	count++;
+	// Move right so increment counter  
+	direction_counter++;
 
-	// if at the left end, start from the right
-	if (count == 10) {
-		count = 0;
-	}
+	// Switch directions if you are at one side 
+	if (direction_counter == 10) 
+		direction_counter = 0;
 	
+	// First we want to reset the count since we are at the end and then check flag 
 	resetCount();
-		
 	checkFlag();
 
-	// reset flag for timer
-	(*timerStatus) = 0;
+	// We also want to reset the timer back to 0
+	(*timer_status) = 0;
 	
 
-	// checks for edge capture to see if key has been pressed
-	// if key has been pressed, switch directions
-	if (*KEY) {
+	// Check if key is pressed, if it is then we switch directions to opposite direction
+	if (*KEY_ptr) 
 		left = false;
-	}
 
 	resetEdgeCapture();
 }
 
 void goingRight(){
 	// display the LED at the correct place
-	*LED = 1 << count;
+	*LED_ptr = 1 << direction_counter;
 
-	// move to the left
-	count--;
+	// Move to the left so decrement counter 
+	direction_counter--;
 
-	// if at the left end, start from the right
-	if (count == -1) {
-		count = 10;
+	// Switch directions if you are at one side 
+	if (direction_counter == -1) {
+		direction_counter = 10;
 	}
 
 	resetCount();
@@ -95,11 +87,11 @@ void goingRight(){
 	checkFlag();
 
 	// reset flag for timer
-	(*timerStatus) = 0;
+	(*timer_status) = 0;
 
 	// checks for edge capture to see if key has been pressed
 	// if key has been pressed, switch directions
-	if (*KEY) {
+	if (*KEY_ptr) {
 		left = true;
 	}
 
@@ -113,6 +105,7 @@ int main(void) {
 			goingLeft();
 		}
 		
+		// If not left, that means we are going right 
 		while (!left) {
 			goingRight();
 		}
